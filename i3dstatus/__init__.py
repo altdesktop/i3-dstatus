6a9be0777c82ec74374c1db91be0a50a9fc04592
Jamie Bliss <astronouth7303@gmail.com>
2015-04-10 04:58:37 -0400

    Run arguments as well as config

--- i3dstatus/__init__.py
+++ i3dstatus/__init__.py
@@ -6,7 +6,9 @@ import sys
 from .service import DStatusService
 from .procman import run_from_config
 
+
 def start():
+    # FIXME: Use actual argument parsing
     DBusGMainLoop(set_as_default=True)
 
     try:
@@ -16,7 +18,7 @@ def start():
         config = {}
 
     manager = DStatusService(config)
-    run_from_config(config)
+    run_from_config(config, sys.argv[1:])
 
     sys.stdout.write('{"version":1}\n[\n[]\n')
     # sys.stdout.write('{"version":1, "click_events":true}\n[\n[]\n')
