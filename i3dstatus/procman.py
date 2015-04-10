2ebdfd4b49986276ffb095749994a1970dffcb76
Jamie Bliss <astronouth7303@gmail.com>
2015-04-10 03:42:37 -0400

    First stab at process "manager"

---
+++ i3dstatus/procman.py
@@ -0,0 +1,57 @@
+"""
+Responsible for starting processes.
+"""
+import os.path
+import pkg_resources
+import subprocess
+import sys
+
+
+def find_generator(name):
+    """
+    Find a generator and return the file name.
+
+    1. If it's an absolute path (with ~ expansions), return resolved name
+    2. If it's one of the bundled generators, resolve it
+    3. Otherwise, assume it's on the PATH and return as-is
+    """
+    # Is this an absolute name?
+    fullname = os.path.expanduser(name)
+    if os.path.isabspath(fullname) and os.path.exists(fullname):
+        return fullname
+
+    # Is it one of the included generators?
+    pkgname = pkg_resources.resource_filename(__name__, 'generators/{}'.format(name))
+    if os.path.exists(pkgname):
+        return name
+
+    # Assume it's on the PATH
+    return name
+
+
+def start_generator(name):
+    """
+    Start a generator, given its partial name.
+
+    * Closes stdin
+    * Redirects stdout to stderr (so that it doesn't conflict with i3bar)
+
+    Returns a Popen object of the started process.
+    """
+    exename = find_generator(name)
+    proc = subprocess.Popen(
+        [exename],
+        stdin=subprocess.PIPE, stdout=sys.stderr)
+    proc.stdin.close()
+    return proc
+
+
+def run_from_config(config):
+    """
+    Entry point from startup.
+
+    Loads a list of generators to start from 'generators'
+    """
+    if 'generators' in config:
+        for gen in config['generators']:
+            start_generator(gen)
