1bf9960d1904fa950035142028bf3854a6b3ace3
Jamie Bliss <astronouth7303@gmail.com>
2015-04-10 03:56:59 -0400

    Actually use the process manager

--- i3dstatus/__init__.py
+++ i3dstatus/__init__.py
@@ -4,7 +4,7 @@ from dbus.mainloop.glib import DBusGMainLoop
 import yaml
 import sys
 from .service import DStatusService
-
+from .procman import run_from_config
 
 def start():
     DBusGMainLoop(set_as_default=True)
@@ -16,6 +16,7 @@ def start():
         config = {}
 
     manager = DStatusService(config)
+    run_from_config(config)
 
     sys.stdout.write('{"version":1}\n[\n[]\n')
     # sys.stdout.write('{"version":1, "click_events":true}\n[\n[]\n')
